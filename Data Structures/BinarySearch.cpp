d80943094f7d90b56ca2cb8bf27bf9b5104b0805
Chitsimran Singh <49387880+chitsimranSingh@users.noreply.github.com>
2022-05-16 22:06:08 +0530

    Delete BinarySearch.cpp

--- Data Structures/BinarySearch.cpp
+++
@@ -1,9 +0,0 @@
-int bs(vector<ll> &v, int l, int r, ll key) {
-    while (l <= r && r >= 0) {
-        int mid = l + (r-l)/2;
-        if (v[mid] == key) return mid;
-        if (v[mid] < key) l = mid + 1;
-        else r = mid - 1;
-    }
-    return -1;
-}
